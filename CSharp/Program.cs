2a8a8c015bbe32f05da799fc3e95b59524d53f13
Santiago Degetau Sada <santiago.degetau@icloud.com>
2020-04-27 21:05:22 -0600

    Removed namespace from Program.cs

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,32 +1,29 @@
-﻿namespace CodingPlayground
+﻿public class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-
-        }
-
-        #region Sorting
-
-        // System.Random rand = new System.Random();
-        // List<int> list = new List<int>(500);
-        // for (int i = 0; i < list.Capacity; i++) list.Add(rand.Next(0, 1001));
-        // var data = list.ToArray();
-
-        // int[] data = { 64, 25, 12, 22, 11, 1 };
-        // System.Console.WriteLine($"Original => [ {string.Join(", ", data)} ]");
-
-        // var bubble = Sorting.BubbleSort(data);
-        // var selection = Sorting.SelectionSort(data);
-        // var insertion = Sorting.InsertionSort(data);
-
-        // var resultList = new List<SortResult<int>>(bubble, selection, insertion);
-        // resultList.ForEach((e) =>
-        // {
-        //     System.Console.WriteLine($"{e.name} => {e.iterations}");
-        // });
-
-        #endregion
+        System.Console.WriteLine(234);
     }
+
+    #region Sorting
+
+    // System.Random rand = new System.Random();
+    // List<int> list = new List<int>(500);
+    // for (int i = 0; i < list.Capacity; i++) list.Add(rand.Next(0, 1001));
+    // var data = list.ToArray();
+
+    // int[] data = { 64, 25, 12, 22, 11, 1 };
+    // System.Console.WriteLine($"Original => [ {string.Join(", ", data)} ]");
+
+    // var bubble = Sorting.BubbleSort(data);
+    // var selection = Sorting.SelectionSort(data);
+    // var insertion = Sorting.InsertionSort(data);
+
+    // var resultList = new List<SortResult<int>>(bubble, selection, insertion);
+    // resultList.ForEach((e) =>
+    // {
+    //     System.Console.WriteLine($"{e.name} => {e.iterations}");
+    // });
+
+    #endregion
 }
