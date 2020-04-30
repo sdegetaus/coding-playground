3b5606b46202462b086aa7b7183e50feafdf3001
Santiago Degetau Sada <santiago.degetau@icloud.com>
2020-04-30 11:42:10 -0600

    Remove Mathematical script (moved to Math class) + add math functions

--- CSharp/Classes/Utility/Math.cs
+++ CSharp/Classes/Utility/Math.cs
@@ -2,6 +2,11 @@ namespace CodingPlayground
 {
     public static class Math
     {
+        public const double PI = 3.14159265358979323846;
+
+        public static double Factorial(int n) => (n == 0) ? 1 : n * Factorial(n - 1);
+        public static int Fibonacci(int n) => (n <= 1) ? n : Fibonacci(n - 1) + Fibonacci(n - 2);
+
         public static int Abs(int value) => (value < 0) ? value * -1 : value;
         public static float Abs(float value) => (value < 0) ? value * -1 : value;
 
@@ -20,6 +25,18 @@ namespace CodingPlayground
             }
             return result;
         }
+
+        // TODO: implement Taylor Series
+        public static double Cos(double a) => System.Math.Cos(a);
+        public static double Sin(double a) => System.Math.Sin(a);
+        public static double Tan(double a) => System.Math.Tan(a);
+
+        #region Extensions
+
+        public static double ToRadians(this double v) => (PI / 180d) * v;
+        public static double ToDegrees(this double v) => v * 180d / PI;
+
+        #endregion
 
     }
 }
