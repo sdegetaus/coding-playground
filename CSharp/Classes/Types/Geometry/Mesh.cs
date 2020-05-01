c826d9ab53df69fdfda86e5272a69c74fefb8e3d
Santiago Degetau Sada <santiago.degetau@icloud.com>
2020-05-01 10:23:33 -0600

    Load from OBJ file

--- CSharp/Classes/Types/Geometry/Mesh.cs
+++ CSharp/Classes/Types/Geometry/Mesh.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+
 namespace CodingPlayground
 {
     public struct Mesh
@@ -24,6 +27,52 @@ namespace CodingPlayground
         public Mesh(int capacity)
         {
             this.triangles = new List<Triangle>(capacity);
+        }
+
+        #endregion
+
+        #region 
+
+        public bool LoadFromFile(string filename)
+        {
+            using (var file = new StreamReader(filename))
+            {
+                triangles = new List<Triangle>();
+                var vertices = new List<Vector3>();
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    // skip empty lines
+                    if (line.Length == 0) continue;
+
+                    if (line[0] == 'v')
+                    {
+                        var points = line.Split(' ');
+                        vertices.Add(
+                            new Vector3(
+                                float.Parse(points[1]),
+                                float.Parse(points[2]),
+                                float.Parse(points[3])
+                            )
+                        );
+                    }
+
+                    if (line[0] == 'f')
+                    {
+                        var f = line.Split(' ');
+                        int[] faces = {
+                            Convert.ToInt32(f[1]),
+                            Convert.ToInt32(f[2]),
+                            Convert.ToInt32(f[3])
+                        };
+
+                        triangles.Add(
+                            new Triangle(vertices[faces[0] - 1], vertices[faces[1] - 1], vertices[faces[2] - 1])
+                        );
+                    }
+                }
+            }
+            return true;
         }
 
         #endregion
